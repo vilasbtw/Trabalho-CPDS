e2e041841fada4fb5cdd58993f33639eb5f0d6aa
Evelyn Theodoro <karolineevelyn999@gmail.com>
2026-04-28 14:48:52 -0300

    Adicionando arquivo relogio_logico

--- utils/relogio_logico.py
+++ utils/relogio_logico.py
@@ -1,2 +1,16 @@
-# utils/relogio_logico.py
-# Implementacao do Relogio Logico para garantir a sincronizacao de eventos distribuido.
+import threading
+
+class RelogioLogico:
+    def __init__(self):
+        self.tempo = 0
+        self.mutex = threading.Lock()
+
+    def incrementar(self):
+        with self.mutex:
+            self.tempo += 1
+            return self.tempo
+
+    def sincronizar(self, tempo_recebido):
+        with self.mutex:
+            self.tempo = max(self.tempo, tempo_recebido) + 1
+            return self.tempo
