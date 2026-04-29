e91468a076140157888d7e6ded44e4f09ba73f9c
Kaique Vilas Boa Reginaldo <ht3036901@hto.ifsp.edu.br>
2026-04-29 20:26:24 -0300

    Mudando nomes de arquivos

--- client/comunicador.py
+++ client/comunicador.py
@@ -2,26 +2,24 @@ import socket
 import threading
 import json
 from utils.relogio_logico import RelogioLogico
-from crypto.seguranca import MotorSeguranca
+from crypto.criptografia import Criptografia
 
 class ClienteBus:
     def __init__(self):
         self.relogio = RelogioLogico()
-        self.seguranca = MotorSeguranca()
+        self.seguranca = Criptografia()
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.nome = ""
 
     def receber(self):
         while True:
             data = self.socket.recv(4096)
+            if not data: break
             p = json.loads(data.decode('utf-8'))
             self.relogio.sincronizar(p['tp'])
-            msg = self.seguranca.desproteger(p['msg'])
-            print(f"\n[Recebido de {p['prod']} | T:{p['tp']}]: {msg}")
             
-            # Envia ACK para auditoria
-            ack = {'acao': 'ack', 'prod': p['prod'], 'tp': p['tp'], 'tc': self.relogio.incrementar(), 'msg': p['msg']}
-            self.socket.sendall(json.dumps(ack).encode('utf-8'))
+            msg = self.seguranca.decriptar(p['msg'])
+            print(f"\n[Recebido de {p['prod']} | T:{p['tp']}]: {msg}")
 
     def conectar(self):
         self.socket.connect(('127.0.0.1', 5000))
@@ -30,12 +28,23 @@ class ClienteBus:
         threading.Thread(target=self.receber, daemon=True).start()
         
         while True:
-            dest = input("Para (ou 'sair'): ")
-            if dest == 'sair': break
-            txt = input("Mensagem: ")
-            tp = self.relogio.incrementar()
-            p = {'acao': 'enviar', 'tipo': 'unicast', 'dest': dest, 'prod': self.nome, 'tp': tp, 'msg': self.seguranca.proteger(txt)}
-            self.socket.sendall(json.dumps(p).encode('utf-8'))
+            print("\n=== SISTEMA DE MENSAGENS ===")
+            print("1. Enviar Mensagem")
+            print("0. Sair")
+            
+            opcao = input("Opção: ").strip()
+            
+            if opcao == "1":
+                dest = input("Destinatário: ")
+                txt = input("Mensagem: ")
+                tp = self.relogio.incrementar()
+                
+                p = {'acao': 'enviar', 'tipo': 'unicast', 'dest': dest, 'prod': self.nome, 'tp': tp, 'msg': self.seguranca.encriptar(txt)}
+                self.socket.sendall(json.dumps(p).encode('utf-8'))
+            elif opcao == "0":
+                break
+            else:
+                print("Opção inválida.")
 
 if __name__ == "__main__":
     ClienteBus().conectar()
